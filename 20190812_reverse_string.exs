659fe0d1c98b63bc56efa48e86d15af596b01530
Hugo Duksis <duksis@gmail.com>
2019-08-13 10:22:11 +0200

    Add reverse charlist to yesterdays kata

--- 20190812_reverse_string.exs
+++ 20190812_reverse_string.exs
@@ -3,15 +3,25 @@ ExUnit.start()
 defmodule KataTest do
   use ExUnit.Case
 
-  test "custom_reverse string" do
+  test "custom_reverse binary string" do
     assert Kata.custom_reverse("abc") == "cba"
+  end
+
+  test "custom_reverse charlist" do
+    assert Kata.custom_reverse('abc') == 'cba'
   end
 end
 
 defmodule Kata do
-  def custom_reverse(_string, _new_string \\ "")
+  def custom_reverse(string) when is_list(string), do: custom_reverse(string, [])
+  def custom_reverse(string) when is_binary(string), do: custom_reverse(string, "")
+
   def custom_reverse("", new_string), do: new_string
+  def custom_reverse([], new_string), do: new_string
   def custom_reverse(<<head :: binary-size(1)>> <> tail, new_string) do
     custom_reverse(tail, head <> new_string)
+  end
+  def custom_reverse(string=[head | tail], new_string) do
+    custom_reverse(tail, [head | new_string])
   end
 end
--- README.md
+++ README.md
@@ -7,3 +7,12 @@ One Kata a day keeps all the bugs away!
 ## Katas
 
 * Reverse string without built in reveres
+
+
+## Usage
+
+* Adding a new solution
+
+```
+touch $(date +%Y%m%d_reverse_string.exs)
+```
